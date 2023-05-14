ed0c66c57ef7648a13c9668374c39c579d0c3ffa
sunkarivasu <sunkarivasu1@gmail.com>
2023-05-15 03:19:49 +0530

    added red & green dot for offerUsers

--- routes/offers.js
+++ routes/offers.js
@@ -12,18 +12,17 @@ router.route("/").get((req,res)=>
 
 router.route("/previousWinners").get((req,res)=>
 {
-    var today = new Date()
+    console.log("---------------------------------");
+    console.log("Fetching previous winner...");
+    var today = new Date(new Date().getTime() + (1000 * 60 * 60 * 5.5))
     var todaysTime = today.getTime()
-    console.log(todaysTime);
-    var hours = today.getHours()
+    console.log({today});
+    var hours = new Date().getHours()
+    console.log({hours});
     if(hours >= 17)
-    {
         var startDate = new Date(todaysTime - (todaysTime%(1000*60*60*24)) - (1000*60*60*24))
-    }
     else
-    {
         var startDate = new Date(todaysTime - (todaysTime%(1000*60*60*24)) - (1000*60*60*24*2))
-    }
     console.log({startDate});
     Offer.find({
         date:{
@@ -32,17 +31,20 @@ router.route("/previousWinners").get((req,res)=>
     })
     .then((offers) => {
         console.log({offers});
-        res.json(offers)})
+        res.json(offers);
+        console.log("---------------------------------");
+    })
     .catch((err) => res.status(400).json("Error"+err));
+
 });
 
 router.route("/add").post((req,res)=>
 {
-    var todaysTime = new Date().getTime()
+    console.log("---------------------------------");
+    var today = new Date(new Date().getTime() + (1000 * 60 * 60 * 5.5))
+    var todaysTime = today.getTime()
     console.log(todaysTime);
     var todaysDate = new Date(todaysTime - (todaysTime%(1000*60*60*24)))
-    console.log(new Date(todaysTime - (todaysTime%(1000*60*60*24))));
-    console.log(new Date(todaysTime - (todaysTime%(1000*60*60*24))) + (1000*60*60*5.5));
     var newOffer = new Offer ({
         productName:req.body.productName,
         worth:req.body.price,
@@ -52,42 +54,59 @@ router.route("/add").post((req,res)=>
     });
     console.log(newOffer);
     newOffer.save()
-    .then(() => res.json("offer added"))
+    .then(() => {
+        res.json("offer added");
+        console.log("---------------------------------");
+})
     .catch((err) => res.status(400).json("Error"+err));
+
 });
 
-router.route("/generateWinner").get((req,res)=>
-{
-    Offer.find()
-    .then((offers) => res.json(offers))
-    .catch((err) => res.status(400).json("Error"+err));
-});
+// router.route("/generateWinner").get((req,res)=>
+// {
+//     console.log("---------------------------------");
+//     Offer.find()
+//     .then((offers) => res.json(offers))
+//     .catch((err) => res.status(400).json("Error"+err));
+//     console.log("---------------------------------");
+// });
 
 router.route("/idNo/:idNumber").get((req,res) =>
 {
+    console.log("---------------------------------");
     Offer.findById(req.params.idNumber)
-    .then((offer) => {res.json(offer)})
+    .then((offer) => {
+        res.json(offer)
+        console.log("---------------------------------");
+    })
     .catch((err) => {res.status(400).json("Error:"+err)});
 });
 
 router.route("/updateOfferDetails/").put((req,res) =>
 {
+    console.log("---------------------------------");
     console.log(req.body);
     Offer.findByIdAndUpdate(req.body._id,{productName:req.body.productName,worth:req.body.worth,winnerName:req.body.winnerName,image:req.body.image,winnerImage:req.body.winnerImage})
-        .then(() => {console.log("Offer updated successfully");res.send({})})
+        .then(() => {
+            console.log("Offer updated successfully");
+            res.send({});
+            console.log("---------------------------------");
+        })
         .catch((err) => {console.log("Error Occured While updating offer details");})
 });
 
 router.route("/getTodaysWinner").get((req,res) =>
 {
-    console.log("fetching today's offer");
-    var today = new Date()
-    const todayTime = today.getTime()
-    var todayDate = new Date(todayTime - (todayTime%(1000 * 60 * 60 * 24)))
-    console.log(todayDate);
-    Offer.findOne({date:todayDate})
+    console.log("---------------------------------");
+    console.log("fetching today's winner");
+    var today = new Date(new Date().getTime() + (1000 * 60 * 60 * 5.5))
+    var todaysTime = today.getTime()
+    var todaysDate = new Date(todaysTime - (todaysTime%(1000 * 60 * 60 * 24)))
+    console.log(today,todaysDate);
+    Offer.findOne({date:todaysDate})
     .then((offer) =>
     {
+        console.log("---------------------------------");
         return res.json(offer)
     })
     .catch((err) =>
@@ -97,115 +116,124 @@ router.route("/getTodaysWinner").get((req,res) =>
 })
 
 router.route("/generateTodaysWinner").get((req,res) =>{
-            console.log("generating todays winner");
-            var presentDate = new Date();
-            var presentDateISO = new Date().toISOString();
-            console.log(presentDate,presentDateISO);
-            OfferUser.find(
-                {
-                    endDate:{
-                        $gte:presentDate
-                    },
-                    startDate:{
-                        $lte:presentDate
-                    },
-                    alreadyWinner:false
-                }
-            )
-            .then((offerUsers) =>
+        console.log("---------------------------------");
+        console.log("generating todays winner");
+        var TodaysDate = new Date(new Date().getTime() + (1000 * 60 * 60 * 5.5));
+        var todaysTime = TodaysDate.getTime()
+        var today = new Date(todaysTime - (todaysTime%(1000 * 60 * 60 * 24))) 
+        OfferUser.find(
             {
-                console.log({offerUsers});
-                var numberOfUsers = offerUsers.length
-                if(numberOfUsers == 0)
-                {
-                    console.log("No offer users found");
-                }
-                else
-                {
-                    var winnerIndex = Math.floor(Math.random() * (numberOfUsers))
-                    var winner = offerUsers[winnerIndex]
-                    console.log("winner:",winner);
-                    console.log("date:",presentDate);
-                    var exactOffer;
-                    var todayTime = new Date().getTime()
-                    var startOfToday = new Date(todayTime - (todayTime%(1000*60*60*24)))
-                    console.log({startOfToday});
-                    Offer.findOne(
-                        {
-                            date:startOfToday,
-                        }
-                    )
-                    .then((offer) =>
-                    {
-                        console.log({offer});
-                        // for(var i=0;i<offers.length;i++)
-                        // {
-                        //     var todayTime = new Date().getTime()
-                        //     var endOfToday = (startOfToday + (1000*60*60*24))
-                        //     if(offers[i].date.getTime() >= startOfToday && offers[i].date.getTime() <= endOfToday)
-                        //     {
-                        //         exactOffer = offers[i]
-                        //         break
-                        //     }
-                        // }
-                        if(!offer)
-                        {
-                            // send message to admin & developer that no offer is added
-                            console.log("No offer added...");
-                        }
-                        else
-                        {
-                            //send message to admin & developer that winner is genderated
-                            Offer.findOneAndUpdate({date:startOfToday},{winnerName:winner.name,winnerPhoneNumber:winner.phoneNumber})
-                            .then(() =>
-                            {
-                                OfferUser.findOneAndUpdate({
-                                       _id:winner._id 
-                                },{
-                                    alreadyWinner:true
-                                })
-                                .then((offer) =>{
-                                    res.send({});
-                                    console.log("alreadyWinner attribute is set");
-                                    console.log("winner generated successfully...");
-                                })
-                                .catch((err) =>{
-                                    console.log("Error occured while setting alreadyWinner attribute..",err);
-                                })
-                            })
-                            .catch((err) =>
-                            {
-                                console.log("Error occured while generating winner...",err);
-                            })
-                        }
-                    })
-                    .catch((err) =>
-                    {
-                        console.log("Error occured while generating winner...",err);
-                    })
-                }
-                
-            })
-            .catch((err) =>
+                endDate:{
+                    $gte:today
+                },
+                startDate:{
+                    $lte:today
+                },
+                alreadyWinner:false
+            }
+        )
+        .then((offerUsers) =>
+        {
+            console.log({offerUsers});
+            var numberOfUsers = offerUsers.length
+            if(numberOfUsers == 0)
             {
-                console.log("Error occured while fetchnig offerUsers count",err);
-            })
+                console.log("No offer users found");
+            }
+            else
+            {
+                var winnerIndex = Math.floor(Math.random() * (numberOfUsers))
+                var winner = offerUsers[winnerIndex]
+                console.log("winner:",winner);
+                console.log("date:",today);
+                // var todayTime = new Date().getTime()
+                // var startOfToday = new Date(todayTime - (todayTime%(1000*60*60*24)))
+                // console.log({startOfToday});
+                Offer.findOne(
+                    {
+                        date:today,
+                    }
+                )
+                .then((offer) =>
+                {
+                    console.log({offer});
+                    // for(var i=0;i<offers.length;i++)
+                    // {
+                    //     var todayTime = new Date().getTime()
+                    //     var endOfToday = (startOfToday + (1000*60*60*24))
+                    //     if(offers[i].date.getTime() >= startOfToday && offers[i].date.getTime() <= endOfToday)
+                    //     {
+                    //         exactOffer = offers[i]
+                    //         break
+                    //     }
+                    // }
+                    if(!offer)
+                    {
+                        // send message to admin & developer that no offer is added
+                        console.log("No offer added...");
+                    }
+                    else
+                    {
+                        //send message to admin & developer that winner is genderated
+                        Offer.findOneAndUpdate({date:today},{winnerName:winner.name,winnerPhoneNumber:winner.phoneNumber})
+                        .then(() =>
+                        {
+                            OfferUser.findOneAndUpdate({
+                                    _id:winner._id 
+                            },{
+                                alreadyWinner:true
+                            })
+                            .then((offerUser) =>{
+                                console.log("alreadyWinner attribute is set");
+                                console.log("winner generated successfully...");
+                                console.log("---------------------------------");
+                                res.send({});
+                            })
+                            .catch((err) =>{
+                                res.status(400).json("Error"+err);
+                                console.log("Error occured while setting alreadyWinner attribute..",err);
+                            })
+                        })
+                        .catch((err) =>
+                        {
+                            res.status(400).json("Error"+err);
+                            console.log("Error occured while generating winner...",err);
+                        })
+                    }
+                })
+                .catch((err) =>
+                {
+                    res.status(400).json("Error"+err);
+                    console.log("Error occured while generating winner...",err);
+                })
+            }
+            
+        })
+        .catch((err) =>
+        {
+            res.status(400).json("Error"+err)
+            console.log("Error occured while fetchnig offerUsers count",err);
+        })
 })
 
 router.route("/getYesterdaysWinner").get((req,res) =>
 {
-    var today = new Date()
+    console.log("---------------------------------");
+    console.log("fetching yesterday's winner");
+    var today = new Date(new Date().getTime() + (1000 * 60 * 60 * 5.5))
+    console.log({today});
     var todayTime = today.getTime()
     console.log(today);
-    var yesterday = new Date(todayTime - (todayTime%(1000 * 60 * 60 * 24)) - (1000 * 60 * 60 *24))
+    var yesterday = new Date(todayTime - (todayTime%(1000 * 60 * 60 * 24))  - (1000 * 60 * 60 *24))
     console.log({yesterday});
     Offer.findOne({
         date: yesterday,
     }) 
     .then((offer) =>
     {
+        console.log("---------------------------------");
         console.log(offer);
-        return res.json(offer)
+        res.json(offer)
     })
     .catch((err) =>
     {
@@ -215,26 +243,23 @@ router.route("/getYesterdaysWinner").get((req,res) =>
 
 router.route("/getTodaysOffer").get((req,res) =>
 {
+    console.log("---------------------------------");
     console.log("fetching today's offer");
-    var today = new Date()
-    console.log(today);
-    var todayStartTime = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var todayEndTime = new Date(today.getFullYear(),today.getMonth(),today.getDate()+1)
-    console.log(todayEndTime,todayStartTime);
-    Offer.findOne({date:{
-        $gte : todayStartTime,
-        $lte : todayEndTime 
-    }})
+    var today = new Date(new Date().getTime() + (1000 * 60 * 60 * 5.5))
+    var todaysTime = today.getTime()
+    var todaysDate = new Date(todaysTime - (todaysTime%(1000 * 60 * 60 * 24)))
+    console.log(today,todaysDate);
+    Offer.findOne({date:todaysDate})
     .then((offer) =>
     {
-        return res.json(offer)
+        console.log("---------------------------------");
+        res.json(offer)
     })
     .catch((err) =>
     {
         console.log("Error occured while fetching today's winner",err);
     })
 })
-
 
 
 module.exports=router;
