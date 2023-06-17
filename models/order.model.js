b96bc42d0320d9eaa22014c004530e82c17646a1
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.com>
2023-06-17 22:41:21 +0530

    [backend] updated order model

--- models/order.model.js
+++ models/order.model.js
@@ -1,14 +1,38 @@
-var mongoose = require("mongoose");
-var orderSchema = new mongoose.Schema(
-    {
-        orderId:Number,
-        userId:mongoose.Schema.ObjectId,
-        amount:Number,
-        status:String,
-        orderItems:[mongoose.Schema.ObjectId],
-    }
-);
+const mongoose = require("mongoose");
 
-var Order = mongoose.model("order",orderSchema);
+const orderSchema = new mongoose.Schema({
+    orderId: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    userId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "user",
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    status: {
+        type: String,
+        required: true,
+        trim: true,
+        enum: ["pending", "confirmed", "delivered", "cancelled"],
+        default: "pending"
+    },
+    orderItems: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "orderProduct",
+        required: true
+    }],
+}, { timestamps: true });
+
+const Order = mongoose.model("order", orderSchema);
 
 module.exports = Order;
