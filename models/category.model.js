176e9881820b4f27985c032d1e83654bdc1915e8
Dhamareshwarakumar Gandikota <damareshwarakumar@gmail.com>
2023-06-17 22:34:28 +0530

    [backend] updated category model

--- models/category.model.js
+++ models/category.model.js
@@ -1,15 +1,28 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-var categorySchema = new mongoose.Schema(
-    {
-        categoryName:String,
-        categoryImage:String,
-        subCategoryList:[
-            String
-        ]
-    }
-);
+const categorySchema = new mongoose.Schema({
+    categoryName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    categoryImage: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    subCategoryList: [{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 255
+    }]
+}, { timestamps: true });
 
-var Category = mongoose.model("category",categorySchema);
+var Category = mongoose.model("category", categorySchema);
 
-module.exports=Category;
+module.exports = Category;
