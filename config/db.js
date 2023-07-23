6df2f459d8ebcbaced42dd2023d7ef144d71d773
sunkarivasu <sunkarivasu1@gmail.com>
2023-07-23 23:13:40 +0530

    updated databse configuration to maintain seperate database for prod & dev

--- config/db.js
+++ config/db.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-    .then(() => console.log(`⚡[server][${process.env.MONGO_ENV}-env] Database connection established`))
-    .catch(err => {
-        console.error(`⚡[server][${process.env.MONGO_ENV}-env] Database connection error: ${err.message}`);
-        process.exit(1);
-    });
+if (process.env.NODE_ENV === 'production') {
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+        .then(() => console.log(`⚡[server][${process.env.MONGO_ENV}-${process.env.NODE_ENV}] Database connection established`))
+        .catch(err => {
+            console.error(`⚡[server][${process.env.MONGO_ENV}-env] Database connection error: ${err.message}`);
+            process.exit(1);
+        });
+}
+else {
+    mongoose.connect('mongodb://127.0.0.1:27017/CitySuperMarketDB')
+        .then(() => console.log(`⚡[server][${process.env.MONGO_ENV}-${process.env.NODE_ENV}] Database connection established`))
+        .catch(err => {
+            console.error(`⚡[server][${process.env.MONGO_ENV}-${process.env.NODE_ENV}] Database connection error: ${err.message}`);
+            process.exit(1);
+        })
+}
